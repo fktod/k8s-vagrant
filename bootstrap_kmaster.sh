ddc3ba095c07389e51dbf9d8204ba7e017be85ef
chenchen <77219776@qq.com>
2023-11-04 15:08:59 +0800

    Update bootstrap_kmaster.sh

--- bootstrap_kmaster.sh
+++ bootstrap_kmaster.sh
@@ -7,7 +7,7 @@ echo "[TASK 2] Initialize Kubernetes Cluster"
 kubeadm init --apiserver-advertise-address=172.16.16.100 --pod-network-cidr=192.168.0.0/16 >> /root/kubeinit.log 2>/dev/null
 
 echo "[TASK 3] Deploy Calico network"
-kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.24/manifests/calico.yaml >/dev/null 2>&1
+kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.25/manifests/calico.yaml >/dev/null 2>&1
 
 echo "[TASK 4] Generate and save cluster join command to /root/joincluster.sh"
 kubeadm token create --print-join-command > /root/joincluster.sh 2>/dev/null
